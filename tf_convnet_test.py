b191c2cf34a28cfb1a756670220879632461b850
Win Woo <wwoo@wwoo-macbookpro.roam.corp.google.com>
2016-09-24 21:33:39 +1000

    Updated code to support multi-channel convolutions

--- tf_convnet_test.py
+++ tf_convnet_test.py
@@ -27,7 +27,7 @@ KEEP_PROB = 0.75
 # image parameters
 IMAGE_SIZE = 150
 IMAGE_RESIZE_FACTOR = 1
-IMAGE_CHANNELS = 1
+IMAGE_CHANNELS = 3
 
 def get_image_label_list(image_label_file):
     filenames = []
@@ -61,22 +61,24 @@ def inputs(train_file, batch_size=TRAIN_BATCH_SIZE, num_epochs=TRAIN_EPOCHS):
     image_batch, label_batch = tf.train.batch([image, label],
         batch_size=batch_size)
 
-    return preprocess_images(image_batch), tf.one_hot(label_batch, NUM_CLASSES)
+    return preprocess_images(image_batch), tf.one_hot(tf.to_int64(label_batch),
+        NUM_CLASSES, on_value=1.0, off_value=0.0)
 
 def conv2d(x, W, b, strides=1):
     # Conv2D wrapper, with bias and relu activation
-    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding='SAME')
+    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1],
+        padding='SAME')
     x = tf.nn.bias_add(x, b)
     return tf.nn.relu(x)
 
 def maxpool2d(x, k=2, layer=""):
     # MaxPool2D wrapper
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1],
-                          padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1],
+        strides=[1, k, k, 1], padding='SAME')
 
 def conv_net(x, weights, biases, image_size, keep_prob=KEEP_PROB):
-    # Reshape image to IMAGE_SIZE / IMAGE_RESIZE_FACTOR
-    x = tf.reshape(x, shape=[-1, image_size, image_size, 1])
+    # Reshape the images to image_size x image_size
+    x = tf.reshape(x, shape=[-1, image_size, image_size, IMAGE_CHANNELS])
 
     # Convolution and max pooling layers
     # Each max pooling layer reduces dimensionality by 2
@@ -114,7 +116,33 @@ def conv_net(x, weights, biases, image_size, keep_prob=KEEP_PROB):
         # Output, class prediction
         out = tf.add(tf.matmul(fc1, weights['out']), biases['out'])
 
+    # maxpool_slice = tf.slice(conv1, [0, 0, 0, 0], [TRAIN_BATCH_SIZE, 75, 75, 1])
+    # maxpool_sample = tf.image_summary("maxpool_sample", maxpool_slice, max_images=10)
+
     return out
+
+def generate_image_summary(x, weights, biases, step, image_size=IMAGE_SIZE):
+    # Reshape the images to image_size x image_size
+    x = tf.reshape(x, shape=[-1, image_size, image_size, IMAGE_CHANNELS])
+
+    with tf.name_scope('generate_image_summary'):
+        x = tf.nn.conv2d(x, weights['wc1'], strides=[1, 1, 1, 1],
+            padding='SAME')
+        x = tf.nn.bias_add(x, biases['bc1'])
+        x_slice = tf.slice(x, [0, 0, 0, 0],
+            [TRAIN_BATCH_SIZE, image_size, image_size, 1])
+        conv_summary = tf.image_summary("img_conv_{:05d}".format(step),
+            x_slice, max_images=1)
+        relu_summary = tf.image_summary("img_relu_{:05d}".format(step),
+            tf.nn.relu(x_slice), max_images=1)
+
+        #conv = conv2d(x, weights['wc1'], biases['bc1'])
+        #conv_slice = tf.slice(conv, [0, 0, 0, 0],
+        #    [TRAIN_BATCH_SIZE, image_size, image_size, 1])
+        #image_summary = tf.image_summary("conv_{:05d}".format(step),
+        #    conv_slice, max_images=1)
+
+    return conv_summary, relu_summary
 
 def preprocess_images(image_batch, resize_factor=IMAGE_RESIZE_FACTOR):
     new_image_size = int(round(IMAGE_SIZE / resize_factor))
@@ -143,27 +171,31 @@ def main(argv=None):
     # Store weights for our convolution & fully-connected layers
     with tf.name_scope('weights'):
         weights = {
-            # 5x5 conv, 1 input, 32 outputs
-            'wc1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
-            # 5x5 conv, 32 inputs, 64 outputs
-            'wc2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
-            # 5x5 conv, 64 inputs, 128 outputs
-            'wc3': tf.Variable(tf.random_normal([5, 5, 64, 128])),
-            # 5x5 conv, 128 inputs, 256 outputs
-            'wc4': tf.Variable(tf.random_normal([5, 5, 128, 256])),
-            # fully connected, 19*19*256 inputs, 1024 outputs
-            'wd1': tf.Variable(tf.random_normal([10*10*256, 1024])),
-            # 1024 inputs, 4 class labels (prediction)
-            'out': tf.Variable(tf.random_normal([1024, NUM_CLASSES]))
+            # 5x5 conv, 1 * IMAGE_CHANNELS input, 32 * IMAGE_CHANNELS outputs
+            'wc1': tf.Variable(tf.truncated_normal([5, 5, 1 * IMAGE_CHANNELS,
+                32 * IMAGE_CHANNELS])),
+            # 5x5 conv, 32 * IMAGE_CHANNELS inputs, 64 * IMAGE_CHANNELS outputs
+            'wc2': tf.Variable(tf.truncated_normal([5, 5, 32 * IMAGE_CHANNELS,
+                64 * IMAGE_CHANNELS])),
+            # 5x5 conv, 64 * IMAGE_CHANNELS inputs, 128 * IMAGE_CHANNELS outputs
+            'wc3': tf.Variable(tf.truncated_normal([5, 5, 64 * IMAGE_CHANNELS,
+                128 * IMAGE_CHANNELS])),
+            # 5x5 conv, 128 * IMAGE_CHANNELS inputs, 256 * IMAGE_CHANNELS outputs
+            'wc4': tf.Variable(tf.truncated_normal([5, 5, 128 * IMAGE_CHANNELS,
+                256 * IMAGE_CHANNELS])),
+            # fully connected, 19 * 19 * 256 * IMAGE_CHANNELS inputs, 1024 outputs
+            'wd1': tf.Variable(tf.truncated_normal([10 * 10 * 256 * IMAGE_CHANNELS, 1024])),
+            # 1024 inputs, 2 class labels (prediction)
+            'out': tf.Variable(tf.truncated_normal([1024, NUM_CLASSES]))
         }
 
     # Store biases for our convolution and fully-connected layers
     with tf.name_scope('biases'):
         biases = {
-            'bc1': tf.Variable(tf.random_normal([32])),
-            'bc2': tf.Variable(tf.random_normal([64])),
-            'bc3': tf.Variable(tf.random_normal([128])),
-            'bc4': tf.Variable(tf.random_normal([256])),
+            'bc1': tf.Variable(tf.random_normal([IMAGE_CHANNELS * 32])),
+            'bc2': tf.Variable(tf.random_normal([IMAGE_CHANNELS * 64])),
+            'bc3': tf.Variable(tf.random_normal([IMAGE_CHANNELS * 128])),
+            'bc4': tf.Variable(tf.random_normal([IMAGE_CHANNELS * 256])),
             'bd1': tf.Variable(tf.random_normal([1024])),
             'out': tf.Variable(tf.random_normal([NUM_CLASSES]))
         }
@@ -177,7 +209,7 @@ def main(argv=None):
     # Calculate loss
     with tf.name_scope('cross_entropy'):
         # Define loss and optimizer
-        cost = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(pred, y_))
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y_))
         cost_summary = tf.scalar_summary("cost_summary", cost)
 
     # Run optimizer step
@@ -189,11 +221,15 @@ def main(argv=None):
         correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y_, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
         accuracy_summary = tf.scalar_summary("accuracy_summary", accuracy)
+        w_summary = tf.histogram_summary("weights", weights['wc1'])
+        b_summary = tf.histogram_summary("biases", biases['bc1'])
 
     sess = tf.Session()
 
+    # conv_filter_sample = tf.image_summary("conv_filter_sample",
+    #    tf.reshape(weights['wc1'], [32, 5, 5, 1]), max_images=3)
+
     writer = tf.train.SummaryWriter("./logs", sess.graph)
-    merged = tf.merge_all_summaries()
     init_op = tf.initialize_all_variables()
 
     # we need init_local_op step only on tensorflow 0.10rc due to a regression from 0.9
@@ -217,11 +253,26 @@ def main(argv=None):
 
                 if step % TRAIN_BATCH_SIZE == 0:
                     x, y = sess.run([valid_image_batch, valid_label_batch])
-                    result = sess.run([merged, accuracy], feed_dict={keep_prob: 1.0,
-                        x_: x, y_: y})
-                    summary_str = result[0]
-                    acc = result[1]
-                    writer.add_summary(summary_str, step)
+                    conv_summary, relu_summary = generate_image_summary(x_, weights, biases, step, image_size)
+                    result = sess.run([cost_summary, accuracy_summary, accuracy, conv_summary, relu_summary, w_summary, b_summary],
+                        feed_dict={keep_prob: 1.0, x_: x, y_: y})
+
+                    cost_summary_str = result[0]
+                    accuracy_summary_str = result[1]
+                    acc = result[2]
+                    conv_summary_str = result[3]
+                    relu_summary_str = result[4]
+                    w_summary_str = result[5]
+                    b_summary_str = result[6]
+
+                    # write summaries for viewing in Tensorboard
+                    writer.add_summary(accuracy_summary_str, step)
+                    writer.add_summary(cost_summary_str, step)
+                    writer.add_summary(conv_summary_str, step)
+                    writer.add_summary(relu_summary_str, step)
+                    writer.add_summary(w_summary_str, step)
+                    writer.add_summary(b_summary_str, step)
+
                     print("Accuracy at step %s: %s" % (step, acc))
 
 
